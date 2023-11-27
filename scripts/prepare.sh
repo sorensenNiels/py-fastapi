59b32a97221e303816027bdc011acff7aa000746
Niels P. Sorensen <sorensen.niels@gmail.com>
2023-11-27 19:57:27 +0000

    Poetry on the move

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -6,9 +6,12 @@ python3 -m venv venv
 # Activate the virtual environment
 source venv/bin/activate
 
+# Install Poetry
+pipx install poetry
+
 # Upgrade pip
-pip install --upgrade pip
+# pip install --upgrade pip
 
 # Install dependencies
-pip install -r requirements.txt
-pip install -r dev-requirements.txt
+# pip install -r requirements.txt
+# pip install -r dev-requirements.txt
