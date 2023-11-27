3628c660f89e98d9c78e0e12d805796d0c904dfa
Niels P. Sørensen <niels.p.sorensen@softdesign.dk>
2023-11-27 10:43:25 +0000

    Fixed prepar script

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Update pip
-pip install --upgrade pip
-
 # Create a virtual environment
 python3 -m venv venv
 
 # Activate the virtual environment
 source venv/bin/activate
 
+# Upgrade pip
+pip install --upgrade pip
+
 # Install dependencies
 pip install -r requirements.txt
-
 pip install -r requirements-dev.txt
